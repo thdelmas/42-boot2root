49784211bc27fa15be6095a75c3e3dfe5b94db70
Theophile DELMAS <thdelmas@e1r3p14.42.fr>
2021-07-17 16:08:57 +0200

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VM_NAME='Boot2Root'
+ISO_PATH='/sgoinfre/goinfre/ISO/BornToSecHackMe-v1.1.iso'
+
+if ! [ -e "$ISO_PATH" ]
+then
+	curl -fsSL 'https://projects.intra.42.fr/uploads/document/document/2832/BornToSecHackMe-v1.1.iso' -o "$ISO_PATH"
+fi
+
+# Ensure VM exist or create it
+if ! [ "$(vboxmanage list vms | grep "$VM_NAME")" ]
+then
+	VBoxManage createvm --name "$VM_NAME" --ostype 'Ubuntu_64' --register 
+	VBoxManage storagectl "$VM_NAME" --name IDE --add ide
+	VBoxManage storageattach "$VM_NAME" --storagectl IDE --port 0 --device 0 --type dvddrive --medium "$ISO_PATH"
+else
+	printf "${TC_RED}There is already a VM with the name: ${VM_NAME}${TC_RESET}\n"
+fi
+
+# Ensure VM is up or start it
+if ! [ "$(vboxmanage list runningvms | grep "$VM_NAME")" ]
+then
+	VboxManage startvm "$VM_NAME"
+else
+	printf "${TC_RED}VM with the name: ${VM_NAME} is already running${TC_RESET}\n"
+fi
