a2d519625c1ff9fccf250c1da3f4bbb2cd9fe001
Theophile Delmas <thdelmas@car4s2.42barcelona.com>
2023-09-02 18:36:27 +0200

    🐛 Fix remove vm

--- utils/uninstall.sh
+++ utils/uninstall.sh
@@ -11,7 +11,8 @@ VM_NAME="$1"
 # Ensure VM exist before deleting it
 if [ "$(vboxmanage list vms | grep "$VM_NAME")" ]
 then
-	VBoxManage controlvm "$VM_NAME" poweroff 
+	VBoxManage controlvm "$VM_NAME" poweroff &&
+	sleep 1 &&
 	VBoxManage unregistervm "$VM_NAME" --delete-all 
 else
 	printf "${TC_RED}There is no VM with the name: ${VM_NAME}${TC_RESET}\n"
