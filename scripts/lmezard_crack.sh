47a1983eb4b30cf197fda72408eb3936a3897f77
Theophile Delmas <thdelmas@cbr11s4.42barcelona.com>
2023-09-08 19:49:15 +0200

    update script lmezard

--- scripts/lmezard_crack.sh
+++ scripts/lmezard_crack.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-rm -rf ../main.c
-mkdir -pv ./files
+# Clean
+rm -rf $HOME/ft_fun
+rm -rf $HOME/main.c
+
+# Exttract archive
+tar -xvf fun
+cd ft_fun
+
+# Get file number and extract the code part into a new file
+mkdir -pv $HOME/files
 for f in $(ls *.pcap)
 do
   ARCHIVE_NUMBER="$(cat $f | grep file | sed 's/^.*file//g')"
-  cat $f | sed 's/\/\/file[0-9]*//g' > ./files/"$ARCHIVE_NUMBER"
+  cat $f | sed 's/\/\/file[0-9]*//g' > $HOME/files/"$ARCHIVE_NUMBER"
 done
 
-cd files
-
+# Gather files in order to rebuild the original file
+cd $HOME/files
 for i in $(ls -1 | sort -n)
 do
-  cat $i >> ../../main.c
+  cat $i >> $HOME/main.c
 done
 
-cd ../..
-
+# Compile the program
+cd $HOME
 gcc main.c -o solution && ./solution | grep PASSWORD | rev | cut -d ' ' -f1 | rev > pass
 echo pass file:
 cat pass
 echo Laurie password is:
-cat pass | tr "\n" '@' | sed 's/@//g' | sha256sum | cut -d ' ' -f1
+password="$(cat pass | tr "\n" '@' | sed 's/@//g' | sha256sum | cut -d ' ' -f1)"
+echo $password
